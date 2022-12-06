f00584381bca27e7ebc8d5b338d71aab914651da
Ian Williamson <ian@4of.uk>
2022-12-06 18:04:12 +0000

    fixed yaml

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,7 +1,7 @@
 // after docs
 
-exports.readUsers = async (req, res) => {
-    response
-        .status(201)
-        .send({ message: "done" });
-};
+exports.readUsers = async (req, res) => {};
+exports.readUser = async (req, res) => {};
+exports.createUser = async (req, res) => {};
+exports.updateUser = async (req, res) => {};
+exports.deleteUser = async (req, res) => {};
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,5 +1,11 @@
 const { Router } = require("express");
-const { readUsers } = require("./userController");
+const {
+    readUsers,
+    readUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require("./userController");
 const userRouter = Router();
 
 /**
@@ -37,73 +43,132 @@ const userRouter = Router();
  *                  type: string
  *                  description: The users address
  *                  example: 1 Downing Street
+ *
+ *      ReturnedUser:
+ *          type: object
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: The users Id
+ *                  example: 1
+ *              user_name:
+ *                  type: string
+ *                  description: The username
+ *                  example: ijwilliamson
+ *              email:
+ *                  type: string
+ *                  description: The users email address
+ *                  example: ian@mail.com
+ *              pcd:
+ *                  type: string
+ *                  description: The users postcode
+ *                  example: SW1A 1AA
+ *              name:
+ *                  type: string
+ *                  description: The users name
+ *                  example: Ian Williamson
+ *              address:
+ *                  type: string
+ *                  description: The users address
+ *                  example: 1 Downing Street
+ *              region_id:
+ *                  type: id
+ *                  description: The users region id
+ *                  example: 1
+ 
+ *      NewUser:
+ *          type: object
+ *          properties:
+ *              user_name:
+ *                  type: string
+ *                  description: The username
+ *                  example: ijwilliamson
+ *              email:
+ *                  type: string
+ *                  description: The users email address
+ *                  example: ian@mail.com
+ *              pcd:
+ *                  type: string
+ *                  description: The users postcode
+ *                  example: SW1A 1AA
+ *              name:
+ *                  type: string
+ *                  description: The users name
+ *                  example: Ian Williamson
+ *              address:
+ *                  type: string
+ *                  description: The users address
+ *                  example: 1 Downing Street
+
  */
 
 /**
  * @swagger
- * /user:
+ * /users:
  *  get:
  *      tags:
- *          -   user
- *      description: Use to get a list of users
+ *          - user
+ *      description: Use to get a list of foods
  *      responses:
- *      '200':
- *          description: A successful response
+ *          '200':
+ *              description: A successful response
  *              content:
  *                  application/json:
  *                      schema:
  *                          type: array
  *                          items:
- *                              $ref: '#/components/schemas/User'
- *        '500':
- *          description: An error response
+ *                              $ref: '#/components/schemas/ReturnedUser'
+ *          '500':
+ *              description: An error response
  *              content:
- *               application/json:
- *                  schema:
- *                     type: object
- *                     properties:
- *                      message:
- *                         type: string
- *                       description: The error message
- *                      example: Internal server error
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  description: The error message
+ *                                  example: Internal server error
  */
 userRouter.get("/users", readUsers);
+
 /**
  * @swagger
- * /user/{:id}
- * get:
- *     tags:
- *        - user
- *     description: Use to get a single user
- *     parameters:
- *          - in: path
+ * /user/{:id}:
+ *  get:
+ *      tags:
+ *          - user
+ *      description: Use to get a single user
+ *      parameters:
+ *      -   in: path
  *          name: id
- *      schema:
- *          type: integer
- *           required: true
- *          description: The user id
+ *          schema:
+ *              type: integer
+ *              required: true
+ *              description: The user id
  *          responses:
  *              '200':
  *                  description: A successful response
  *                  content:
- *                  application/json:
- *                  schema:
- *                   $ref: '#/components/schemas/User'
- *               '500':
- *                    description: An error response
- *                       content:
- *                           application/json:
- *                       schema:
- *                            type: object
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/User'
+ *              '500':
+ *                  description: An error response
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
  *                              properties:
- *                          message:
- *                                  type: string
- *                              description: The error message
- *                              example: Internal server error
+ *                                  message:
+ *                                      type: string
+ *                                      description: The error message
+ *                                      example: Internal server error
  *
  *
  */
 userRouter.get("/user/:id", readUser);
+
 /**
  * @swagger
  * /user:
@@ -111,104 +176,106 @@ userRouter.get("/user/:id", readUser);
  *      tags:
  *          - user
  *      description: Use to create a new user
- *          requestBody:
- *              required: true
- *                  content:
- *                      application/json:
- *                          schema:
- *                              $ref: '#/components/schemas/User'
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/NewUser'
  *      responses:
  *          '201':
  *              description: A successful response
  *              content:
  *                  application/json:
  *                      schema:
- *                          $ref: '#/components/schemas/User'
- *          '500':
- *              description: An error response
- *              content:
- *                   application/json:
- *                       schema:
- *                          type: object
- *              properties:
- *                  message:
- *                      type: string
- *                          description: The error message
- *                          example: Internal server error
+ *                          $ref: '#/components/schemas/ReturnedUser'
+ *              '500':
+ *                  description: An error response
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *                                      description: The error message
+ *                                      example: Internal server error
  */
 userRouter.post("/user", createUser);
+
 /**
  * @swagger
- * /user/{:id}
+ * /user/{:id}:
  *  put:
  *      tags:
  *          - user
  *      description: Use to update a user
- *          parameters:
- *              - in: path
+ *      parameters:
+ *      -   in: path
  *          name: id
- *              schema:
+ *          schema:
  *              type: integer
- *              required: true
- *              description: The user id
- *              requestBody:
- *              required: true
- *              content:
+ *          required: true
+ *          description: The user id
+ *      requestBody:
+ *          required: true
+ *          content:
  *              application/json:
- *              schema:
- *              $ref: '#/components/schemas/User'
- *              responses:
- *      '200':
+ *                  schema:
+ *                      $ref: '#/components/schemas/NewUser'
+ *      responses:
+ *          '200':
  *              description: A successful response
  *              content:
- *              application/json:
- *              schema:
- *              $ref: '#/components/schemas/User'
- *      '500':
- *              description: An error response
- *              content:
- *              application/json:
- *              schema:
- *              type: object
- *              properties:
- *              message:
- *              type: string
- *              description: The error message
- *              example: Internal server error
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ReturnedUser'
+ *              '500':
+ *                  description: An error response
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *                                      description: The error message
+ *                                      example: Internal server error
  */
 userRouter.put("/user/:id", updateUser);
+
 /**
  * @swagger
- * /user/{:id}
+ * /user/{:id}:
  *  delete:
  *      tags:
  *          - user
  *      description: Use to delete a user
  *      parameters:
- *          - in: path
- *      name: id
- *      schema:
- *          type: integer
+ *      -   in: path
+ *          name: id
+ *          schema:
+ *              type: integer
  *              required: true
- *      description: The user id
- *          responses:
- *      '200':
- *          description: A successful response
+ *              description: The user id
+ *      responses:
+ *          '200':
+ *              description: A successful response
  *              content:
- *              application/json:
- *                  schema:
- *                      $ref: '#/components/schemas/User'
- *      '500':
- *          description: An error response
- *              content:
- *              application/json:
- *                   schema:
- *                      type: object
- *              properties:
- *              message:
- *              type: string
- *                  description: The error message
- *                  example: Internal server error
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ReturnedUser'
+ *              '500':
+ *                  description: An error response
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  message:
+ *                                      type: string
+ *                                      description: The error message
+ *                                      example: Internal server error
  */
 userRouter.delete("/user/:id", deleteUser);
 
