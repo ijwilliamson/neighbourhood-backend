4f8c76eee167012370ab31c76f7a5d910ff8e2cf
RowDog <mrdiamonddirt@gmail.com>
2022-12-07 10:16:56 +0000

    add controllers

--- src/region/regionController.js
+++ src/region/regionController.js
@@ -1,4 +1,35 @@
 const Region = require("./regionModel");
 
-exports.getRegionByPcd = async (req, res) => {};
-exports.getRegionById = async (req, res) => {};
+exports.getRegionById = async (req, res) => {
+    console.log(req);
+    try {
+        const region = await Region.findByPk(
+            req.params.id,
+            { attributes: ["id", "region_name"] }
+        );
+        if (region) {
+            res.status(200).json(region);
+        } else {
+            res.status(404).send(
+                "Region not found"
+            );
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.getRegionByPcd = async (req, res) => {
+    try {
+        const region = await Region.findOne({});
+        if (region) {
+            res.status(200).json(region);
+        } else {
+            res.status(404).send(
+                "Region not found"
+            );
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
